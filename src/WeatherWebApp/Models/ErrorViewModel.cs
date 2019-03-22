381bb439ee2466240ea5bc724afbd313faab2bd1
Roman Köhler <roman@simplisticmusic.de>
2019-03-22 12:16:22 +0100

    Added weather web app

---
+++ src/WeatherWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeatherWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
